d03583f93387263a8b315330a1cf9c52ef25cb26
MAHONRRIMENDEZ <97697808+MAHONRRIMENDEZ@users.noreply.github.com>
2025-01-18 17:59:11 -0600

    completed

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 
-router.get('/', (req, res) => { res.send('Hello World papu')});
+//router.get('/', (req, res) => { res.send('Hello World papu')});
+
+router.use('/users', require('./users'));
 
 module.exports = router;
