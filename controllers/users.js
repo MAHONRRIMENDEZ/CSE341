66f6468d44a1e550f7112654425d221e9a46e7e1
MAHONRRIMENDEZ <97697808+MAHONRRIMENDEZ@users.noreply.github.com>
2025-02-01 17:52:19 -0600

    1

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,5 @@
 const mongodb = require('../data/database');
-const ObjectId = require('mongodb').ObjectId; //both use mongodb.getDatabase() para acceder a la colección "users" de la base de datos y los camb  formato JSON
+const ObjectId = require('mongodb').ObjectId; //ambos usan mongodb.getDatabase() para acceder a la colección "users" de la base de datos y los camb  formato JSON
 
 const getALL = async (req, res) => { //  obtiene todos los usuarios de la base de datos, los convierte a un arreglo con toArray() 
     const result = await mongodb.getDatabase().db().collection('users').find();
@@ -10,7 +10,7 @@ const getALL = async (req, res) => { //  obtiene todos los usuarios de la base d
 
 };
 
-const getSingle = async (req, res) => { //usca un usuario específico utilizando un ObjectId basado en el id recibido como parámetro en la URL
+const getSingle = async (req, res) => { //busca un usuario específico utilizando un ObjectId basado en el id recibido como parámetro en la URL
     const userId = new ObjectId(req.params.id);
     const result = await mongodb.getDatabase().db().collection('users').find({ _id: userId });
     result.toArray().then((users) => {
